4542e1de4bf8d83e75554e46e9a3d5b153575a96
dchu400 <chudaniel400@gmail.com>
2026-05-20 14:34:49 -0400

    feat: add index.html base HTML web page

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     print("Running")
     app.run()
