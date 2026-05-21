ec3c7a8c371b7308a317fe6247dba5a85ed0b90e
dchu400 <chudaniel400@gmail.com>
2026-05-20 22:20:06 -0400

    refactor: add new method image() which handles the API endpoint /image, which returns an HTTP response of the pyplot of the sine wave with the given frequency

--- app.py
+++ app.py
@@ -9,33 +9,28 @@ app = Flask(__name__)
 
 def sin_wave(ts: np.ndarray, freq: int = 1):
     return np.sin(2 * np.pi * freq * ts)
-    
-@app.route("/", methods=["GET", "POST"])
-def index():
-    ts: np.ndarray = np.linspace(0, 2, 11025)
-    
-    freq: int = 1
-    if request.method == "POST":
-        freq = int(request.form["freq"])
-        print(f"Got frequency {freq}")
-    else:
-        try:
-            freq: int = int(request.args.get("freq", "1"))
-        except ValueError:
-            pass
 
-    ys = sin_wave(ts, freq)
-    fig, ax = plt.subplots()
-    ax.plot(ts, ys)
+
+@app.post("/image")
+def image():
+    freq: int = int(request.form["freq"])
+    ts: np.ndarray = np.linspace(0, 2, 11025)
+    ys: np.ndarray = sin_wave(ts, freq=freq)
 
     # Convert the buffer output into a base 64 string
     buffer = io.BytesIO()
+
+    fig, ax = plt.subplots()
+    ax.plot(ts, ys)
     fig.savefig(buffer, format='png')
     data = base64.b64encode(buffer.getbuffer()).decode("ascii")
-    print(buffer.getbuffer())
+
+    return f"<img id='plot-image' src='data:image/png;base64,{data}'/>"
     
-    figure_html: str = f"<img src='data:image/png;base64,{data}'/>"
-    return render_template("index.html", figure_html=figure_html)
+    
+@app.get("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     print("Running")
