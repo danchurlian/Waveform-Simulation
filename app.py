dca4318c42b5501c4d2dba60526db5ef57fb1fad
dchu400 <chudaniel400@gmail.com>
2026-05-20 21:59:29 -0400

    feat: add text input field that chooses the frequency for the sine wave, write post request handling and HTMX

--- app.py
+++ app.py
@@ -10,15 +10,19 @@ app = Flask(__name__)
 def sin_wave(ts: np.ndarray, freq: int = 1):
     return np.sin(2 * np.pi * freq * ts)
     
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     ts: np.ndarray = np.linspace(0, 2, 11025)
     
     freq: int = 1
-    try:
-        freq: int = int(request.args.get("freq", "1"))
-    except ValueError:
-        pass
+    if request.method == "POST":
+        freq = int(request.form["freq"])
+        print(f"Got frequency {freq}")
+    else:
+        try:
+            freq: int = int(request.args.get("freq", "1"))
+        except ValueError:
+            pass
 
     ys = sin_wave(ts, freq)
     fig, ax = plt.subplots()
